182c9ed50987b355690a53e9c7ac6222dd50a70e
Fzuker97 <86082134+Fzuker97@users.noreply.github.com>
2023-06-21 19:58:36 +0200

    Add files via upload

--- Library/Main.js
+++ Library/Main.js
@@ -13,9 +13,14 @@ openModal.addEventListener('click', () => {
   modal.showModal();
 })
 
-closeModal.addEventListener('click', () => {
-  modal.close();
-})
+closeModal.addEventListener('click', function (event) {
+    event.preventDefault();
+    const bookTitle = title.value;
+    const bookAuthor = author.value;
+    const bookPages = pages.value;
+    addBookToLibrary(bookTitle, bookAuthor, bookPages);
+    modal.close();
+  })
 
 //Array Functions
 let myLibrary = [];
@@ -24,23 +29,29 @@ function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  
+  this.read = false;
 }
 
 function addBookToLibrary(title, author, pages) {
   const newBook = new Book(title, author, pages);
   myLibrary.push(newBook);
-
+  displayLibrary();
 }
 
 // Display Array
 
+function toggleReadStatus(index) {
+  myLibrary[index].read = !myLibrary[index].read;
+  displayLibrary(); 
+}
+
+
 function displayLibrary(){
-  const libraryContainer = document.getElementById('#bookList');
+  const libraryContainer = document.getElementById('bookList');
 
   libraryContainer.innerHTML = "";
 
-  myLibrary.forEach(book => {
+  myLibrary.forEach((book, index) => {
     const card = document.createElement("div");
     card.classList.add("card");
 
@@ -53,13 +64,40 @@ function displayLibrary(){
     const pages = document.createElement("p");
         pages.textContent = "Pages: " + book.pages;
 
+    const check = document.createElement("button");
+    if(book.read) {
+      check.textContent = 'Leido';
+    } else{
+      check.textContent = 'No Leido';
+    }
+    check.addEventListener('click', () => {
+      toggleReadStatus(index);
+    });
+
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = 'Remove';
+    removeButton.addEventListener('click', () => {
+      removeBookFromLibrary(index);
+    })
+
         card.appendChild(title);
         card.appendChild(author);
         card.appendChild(pages);
+        card.appendChild(removeButton);
+        card.appendChild(check);
 
         libraryContainer.appendChild(card);
   });
 }
 
+function removeBookFromLibrary(index){
+  myLibrary.splice(index, 1);
+  displayLibrary();
+}
+
+function changeCheck(){
+  
+}
 
 displayLibrary();
